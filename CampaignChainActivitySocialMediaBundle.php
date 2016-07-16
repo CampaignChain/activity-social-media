11f9a0c234a452069bcd6883325ea672738e4ca6
Sandro Groganz <sandro@campaignchain.com>
2016-07-16 12:32:19 -0700

    CampaignChain/campaignchain#192 Subsume all app-wide parameters under campaignchain_core using tree builder

--- CampaignChainActivitySocialMediaBundle.php
+++ CampaignChainActivitySocialMediaBundle.php
@@ -2,8 +2,13 @@
 
 namespace CampaignChain\Activity\SocialMediaBundle;
 
+use CampaignChain\Activity\SocialMediaBundle\DependencyInjection\CampaignChainActivitySocialMediaExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CampaignChainActivitySocialMediaBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new CampaignChainActivitySocialMediaExtension();
+    }
 }
